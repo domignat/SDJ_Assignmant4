60c86d78ea80e9eeeaa5944d208e25b880c12c2a
Eli3336 <mihai_elisabeta_3@yahoo.com>
2022-11-23 14:13:51 +0100

    create slaughterhouse2

---
+++ SlaugtherHouse2/src/main/java/via/sdj3/slaugtherhouse2/SlaugtherHouse2Application.java
@@ -0,0 +1,13 @@
+package via.sdj3.slaugtherhouse2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlaugtherHouse2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlaugtherHouse2Application.class, args);
+    }
+
+}
---
+++ SlaugtherHouse2/src/test/java/via/sdj3/slaugtherhouse2/SlaugtherHouse2ApplicationTests.java
@@ -0,0 +1,13 @@
+package via.sdj3.slaugtherhouse2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlaugtherHouse2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
